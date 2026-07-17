2996c548a97c8d99f548fe64941ec97ec1079794
Amir H. Ebrahimi <121507867+vision468@users.noreply.github.com>
2026-07-17 13:04:14 +0000

    fix: incoming hello message

--- src/hello.py
+++ src/hello.py
@@ -1,4 +1,1 @@
-print("Hello!")
-print("This is a simple Python script, by vision468 from GitHub.")
-print("It is a try on using GitHub Codespace to create a Python project.")
-print("Feel free to modify and use it as you like.")
+print("Hello World!")
